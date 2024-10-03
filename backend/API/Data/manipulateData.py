2a418ac0d3a94a27e0820575647d6ae21eb7cfe3
KoussayDhifi <koussaydhifi48@gmail.com>
2024-10-03 21:14:51 +0100

    Finish data preprocessing and started with temp predection

--- backend/API/Data/manipulateData.py
+++ backend/API/Data/manipulateData.py
@@ -1,91 +1,37 @@
 import pandas as pd
-import re
-import os
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-#Path of datasets (will change in deployment)
-PATH = r"/mnt/c/Users/kouss/Documents/Stage/ClassroomCondition/backend/API/Data"
+temp = pd.read_csv('classroom1/Temperature.csv')
+hum = pd.read_csv('classroom1/Humidity.csv')
+lum = pd.read_csv('classroom1/Luminance.csv')
+noise = pd.read_csv('classroom1/Noise.csv')
 
-#This function will change in deployment
-def listDirectory():
-    
-    listDir = []
-    
-    for i in os.listdir(PATH):
-        if re.match(r'classroom*',i):
-            listDir.append(i)
-    
-    return listDir
-    
 
-def extractData(time,date):
-    dirs = listDirectory()
-    print(dirs)
-    time = str(time)
-    date = str(date)
-    hour = time[0:2]
-    month = date[5:7]
-    print(hour)
-    print(month)
-    # Construct regex pattern
-    specific_time_pattern = rf'{date[:5]}{month}-[0-9]{{2}} {hour}:[0-5][0-9]:[0-5][0-9]'
+def process_dataframe(df):
+    
+    df['time_of_reading'] = pd.to_datetime(df['time_of_reading'], format="%Y-%m-%d %H:%M:%S")
+    
+    df['month'] = df['time_of_reading'].dt.month
+    df['day'] = df['time_of_reading'].dt.day
+    df['hour'] = df['time_of_reading'].dt.hour
+    df['minute'] = df['time_of_reading'].dt.minute
+
     
     
-    dataMap = {}
-    list_of_variables = ['Noise','Luminance','Humidity', 'Temperature']
-    print(f"Dirs: {dirs}")
-    for directory in dirs:
-        dataMap[directory] = {}
-        for variable in list_of_variables:
-            file_path = f'{PATH}/{directory}/{variable}.csv'
-            if os.path.exists(file_path):
-                dataClass = pd.read_csv(file_path)
-                dataClass['time_of_reading'] = pd.to_datetime(dataClass['time_of_reading'])
-                filtered_data = dataClass[dataClass['time_of_reading'].dt.strftime('%Y-%m-%d %H:%M:%S').str.contains(specific_time_pattern)]
-                filtered_data = filtered_data['reading']
-                dataMap[directory][variable] = filtered_data.mean()
-             
-                
-            else:
-                print(f"File not found: {file_path}")
-    
-    return dataMap
+    df.drop(['time_of_reading', 'unit', 'device_type','time_of_create'], axis=1, inplace=True)
+    df = df.groupby(['month', 'day', 'hour', 'minute']).agg({'reading': 'mean'}).reset_index() #Calculate the avg of redundant values
 
+    return df
 
+temp = process_dataframe(temp)
+hum = process_dataframe(hum)
+lum = process_dataframe(lum)
+noise = process_dataframe(noise)
 
-def sortData(time,date):
-    dataMap = extractData(time,date)
-    classes = list(dataMap)
-    variables = []
-    for i in dataMap:
-        variables.append(dataMap[i])
-    print(classes)
-    print(variables)
+temp.to_csv('newData/temp.csv')
+hum.to_csv('newData/hum.csv')
+lum.to_csv('newData/lum.csv')
+noise.to_csv('newData/noise.csv')
 
-    length = len(classes)
-
-    for i in range(length):
-        for j in range(length-1):
-            if abs(variables[j]['Noise']-variables[j+1]['Noise']) > 10:
-                variables[j],variables[j+1] = variables[j+1],variables[j]
-                classes[j],classes[j+1] = classes[j+1],classes[j]
-
-            elif abs(variables[j]['Luminance'] - 500) > abs(variables[j+1]['Luminance']-500):
-                 variables[j],variables[j+1] = variables[j+1],variables[j]
-                 classes[j],classes[j+1] = classes[j+1],classes[j]
-
-            elif abs(variables[j]['Humidity']-45) > abs(variables[j+1]['Humidity']-45):
-                variables[j],variables[j+1] = variables[j+1],variables[j]
-                classes[j],classes[j+1] = classes[j+1],classes[j]
-
-            elif abs(variables[j]['Temperature']-21) > abs(variables[j+1]['Temperature']-21):
-                variables[j],variables[j+1] = variables[j+1],variables[j]
-                classes[j],classes[j+1] = classes[j+1],classes[j]
-
-
-    sorted_items = dict()
-
-    for i in range(length):
-        sorted_items[classes[i]] = variables[i]
-    
-    return sorted_items
 
